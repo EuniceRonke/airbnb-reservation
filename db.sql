8146e68e0ff501f4fd417aeb257a7152717e4ab5
EuniceRonke <eunicefagbemide@gmail.com>
2025-09-04 16:10:52 +0100

    Add guest_type table to store guest types

--- db.sql
+++ db.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+-- Table: guest_type
+-- This table stores different types of guests that can be included in a booking.
+-- Each row represents a unique guest type (e.g., Adult, Child, VIP).
+
+CREATE TABLE guest_type (
+    id INT PRIMARY KEY AUTO_INCREMENT,  -- Unique identifier for each guest type
+    type_name VARCHAR(255) NOT NULL     -- Name of the guest type
+);
